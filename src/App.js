87ef1a3cb8e76498a90b727ab5acf4926ee4fd9d
dattphan15 <ken.phan@live.ca>
2021-09-04 20:00:47 -0700

    add constructor with props and state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
   render() {
-    return <div className="App"></div>;
+    return <div className="App">hello</div>;
   }
 }
 
